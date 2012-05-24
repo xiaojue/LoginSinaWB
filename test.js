07d7acf7a008ef20f4385cd7153ac566572c6e45
Xiaojue <designsor@gamil.com>
2012-05-24 11:36:59 +0800

    format

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 var LoginSinaWB = require('./LoginSinaWB');
 
-LoginSinaWB.login('username','password',function(err,browser){
-    if(!err){ 
-        browser.visit('http://www.weibo.com',function(){
-           browser.wait(function(){
-                console.log(browser.location.href);
-           }); 
-        });    
-    }
+LoginSinaWB.login('username', 'password', function(err, browser) {
+	if (!err) {
+		browser.visit('http://www.weibo.com', function() {
+			browser.wait(function() {
+				console.log(browser.location.href);
+			});
+		});
+	}
 });
+
